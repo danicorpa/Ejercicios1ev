ba5d20f584f278c58958166c47e0c01caeaf16e5
danicorpa <daniel.martin385@educa.madrid.org>
2022-10-04 20:30:38 +0200

    Ejercicios Clase

---
+++ src/VariablesYOperadores/Ejercicio23.java
@@ -0,0 +1,21 @@
+package VariablesYOperadores;
+
+import java.util.Scanner;
+
+public class Ejercicio23 {
+
+    /*
+    * 23. Realiza un programa que pida un número y un porcentaje, y
+a. Calcule y muestre ese número incrementado en ese porcentaje.
+b. Calcule y muestre ese número decrementado en ese porcentaje.
+    * */
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un numero: ");
+        int num1 = sc.nextInt();
+
+
+    }
+}
